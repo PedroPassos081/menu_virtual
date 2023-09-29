abbd10761efa80b9f44472da34466c59ca70efac
PedroPassos081 <pedro.passos081@gmail.com>
2023-09-29 19:35:07 -0300

    iniciando o app.js do projeto

---
+++ js/app.js
@@ -0,0 +1,29 @@
+$(document).ready( () => {
+    cardapio.eventos.init();
+})
+
+var cardapio = {};
+
+cardapio.eventos = {
+
+    init: () =>{
+
+    }
+}
+
+.cardapio.metodos = {
+
+//obtem a lista de itens do cardápio
+    obterItensCardapio: () => {
+
+        var filtro = MENU['burgers']
+
+        $.each(filtro, (i, e) => {
+
+        })
+    }
+}
+
+cardapio.templates {
+
+};
