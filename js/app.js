f34f9bbaa1149db0d4f3042741748094a22f2d24
Pedro Passos <pedro.passos081@gmail.com>
2023-10-02 16:11:38 -0300

    Deixando o cardapio mais dinamico

--- js/app.js
+++ js/app.js
@@ -1,29 +1,60 @@
-$(document).ready( () => {
-    cardapio.eventos.init();
-})
+$(document).ready(() => {
+  cardapio.eventos.init();
+});
 
 var cardapio = {};
 
 cardapio.eventos = {
+  init: () => {
+    cardapio.metodos.obterItensCardapio();
+  },
+};
 
-    init: () =>{
+cardapio.metodos = {
+  //obtem a lista de itens do cardápio
+  obterItensCardapio: (categoria = "burgers") => {
+    var filtro = MENU[categoria];
+    console.log(filtro);
 
-    }
-}
+    $("#itensCardapio").html("");
 
-.cardapio.metodos = {
+    $.each(filtro, (i, e) => {
+      let temp = cardapio.templates.item
+        .replace(/\${img}/g, e.img)
+        .replace(/\${name}/g, e.name)
+        .replace(/\${price}/g, e.price.toFixed(2).replace(".", ","));
 
-//obtem a lista de itens do cardápio
-    obterItensCardapio: () => {
+      $("#itensCardapio").append(temp);
+    });
 
-        var filtro = MENU['burgers']
+    //remove o ativo
+    $(".container-menu a").removeClass("active");
 
-        $.each(filtro, (i, e) => {
+    //seta o menu para ativo
+    $("#menu-" + categoria).addClass("active");
+  },
+};
 
-        })
-    }
-}
-
-cardapio.templates {
-
+cardapio.templates = {
+  item: `
+    <div class="col-3 mb-5">
+    <div class="card card-item">
+        <div class="img-produto">
+            <img src="\${img}">
+        </div>
+        <p class="title-produto text-center mt-4">
+            <b>\${name}</b>
+        </p>
+        <p class="price-produto text-center">
+            <b>\${price}</b>
+        </p>
+        <div class="add-carrinho">
+            <span class="btn-menos"><i class="fas fa-minus"></i></span>
+            <span class="add-numero-itens">0</span>
+            <span class="btn-mais"><i class="fas fa-plus"></i></span>
+            <span class="btn btn-add"><i class="fas fa-shopping-bag"></i></span>
+        </div>
+    </div>
+</div>
+    `,
 };
