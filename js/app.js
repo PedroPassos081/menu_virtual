043074c65623595da606b11621b749c7c032ad30
PedroPassos081 <pedro.passos081@gmail.com>
2023-10-10 10:50:37 -0300

    Adicionando o envio do pedido para o whatszap

--- js/app.js
+++ js/app.js
@@ -422,6 +422,45 @@ cardapio.metodos = {
     $("#cidadeEndereco").html(
       `${MEU_ENDERECO.cidade}-${MEU_ENDERECO.uf}/ ${MEU_ENDERECO.cep} ${MEU_ENDERECO.complemento}`
     );
+
+    cardapio.metodos.finalizarPedido()
+  },
+
+  //Atualiza o link do botão do whatsapp
+  finalizarPedido: () => {
+
+    if(MEU_CARRINHO.length > 0 && MEU_ENDERECO != null) {
+
+      var texto = 'Olá gostaria de fazer um pedido:'
+      texto += `\n*Itens do pedido:*\n\n\${itens}`
+      texto += '\n*Endereço de entrega:*'
+      texto += `\n${MEU_ENDERECO.endereco}, ${MEU_ENDERECO.numero}, ${MEU_ENDERECO.bairro}`
+      texto += `\n${MEU_ENDERECO.cidade}-${MEU_ENDERECO.uf} / ${MEU_ENDERECO.cep} ${MEU_ENDERECO.complemento}`
+      texto += `\n\n*Total (com entrega): R$ ${(VALOR_CARRINHO + VALOR_ENTREGA).toFixed(2).replace('.', ',')}*`
+   
+      var itens = ''
+   
+      $.each(MEU_CARRINHO, (i, e) => {
+
+        itens += `*${e.qntd}x* ${e.name} ....... R$ ${e.price.toFixed(2).replace('.', ',')} \n`;
+      
+
+        if ((i + 1) == MEU_CARRINHO.length) {
+
+          texto = texto.replace(/\${itens}/g, itens)
+
+          //converte a URL
+          let encode = encodeURI(texto)
+          let URL= `https://wa.me/5581989714251?text=${encode}`
+
+          $("#btnEtapaResumo").attr('href', URL)
+        }
+      
+      
+      })
+   
+    }
+
   },
 
   //mensagens
